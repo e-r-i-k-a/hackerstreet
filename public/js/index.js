0246ffb4c42266cbfcd747dabeaee8939e6ba9b5
Anna Medyukh <annamedyukh@gmail.com>
2018-01-27 16:11:53 -0500

    added dates

--- public/js/index.js
+++ public/js/index.js
@@ -147,7 +147,6 @@ $('.multiple-items').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    // asNavFor: '.slider-for',
     dots: true,
     centerMode: true,
     focusOnSelect: true
